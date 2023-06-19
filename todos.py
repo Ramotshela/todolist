96445cee3cdb8726bdda6dd5dab0d47a4a9521c7
Ramotshela <122275648+Ramotshela@users.noreply.github.com>
2023-06-19 11:06:42 +0200

    Update todos.py

--- todos.py
+++ todos.py
@@ -1,13 +1,21 @@
 todos= []
 
 while True:
-    user=input("enter add,show / remove : ")
+    user=input("enter add,show, remove & edit : ")
     user.strip()
     
     match user:
         case 'add':
             todo=input("enter your todos")
             todos.append(todo)
+        case 'edit':
+            number=input('enter number of entry to be changed')
+            number-=1
+            new_entry=input('enter the replacement entry')
+            todos[number]=new_entry
+            for items in todos:
+                print (items)
+            print('entry has been updated')
         case 'show':
             for items in todos:
                 print (items)
